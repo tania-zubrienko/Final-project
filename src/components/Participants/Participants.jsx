7df91c41bf534c9f840789bf9004c8186ad97e26
Tania Zubrienko <tatiana.zubrienko@gmail.com>
2023-12-04 16:42:09 +0100

    add group

--- src/components/Participants/Participants.jsx
+++ src/components/Participants/Participants.jsx
@@ -9,13 +9,9 @@ const Participants = ({ participants, id, refresh }) => {
 
     const [show, setShow] = useState(false)
     const [friends, setFriends] = useState([])
-    const [selectedFriends, setSelectedFriends] = useState(participants)
-
-    const [state, setState] = useState(false)
-
+    const [members, setMembers] = useState(participants)
 
     useEffect(() => getFriends(), [])
-    useEffect(() => refresh(), [state])
 
     const getFriends = () => {
         userServices
@@ -24,45 +20,42 @@ const Participants = ({ participants, id, refresh }) => {
             .catch(err => console.log(err))
     }
 
-    const handlerAddParticipant = () => {
-        setShow(true)
-        getFriends()
-
-    }
+    const handlerAddParticipant = () => setShow(true)
 
     const handleClose = () => {
-        getFriends()
-        setShow(false)
-        setState(!state)
-    }
-
-    const handleAddToGroup = () => {
-        tripServices
-            .addParticipants(selectedFriends, id)
-            .then(refresh())
-            .catch(err => console.log(err))
         refresh()
         setShow(false)
-        setState(!state)
-
     }
-    const [checked, setChecked] = useState('unChecked')
 
-    const markChecked = e => {
+    const addMember = (e) => {
         const { value } = e.target
-        setSelectedFriends([...selectedFriends, value])
-        setState(!state)
+        setMembers([...members, value])
+        refresh()
+    }
+
+    const updateGroup = () => {
+        const newMembers = Array.from(new Set(members))
+        tripServices
+            .addParticipants(newMembers, id)
+            .then(setShow(false))
+            .catch(err => console.log(err))
+        setMembers(participants)
+        refresh()
 
     }
 
+    useEffect(() => refresh(), [show])
     return (
         <div className="Participants">
             {participants.length > 0 &&
-                participants.map(e =>
-                    <div className="userCard" key={e._id}>
+                participants.map(e => {
+
+                    return (<div className="userCard" key={e._id}>
                         <img src={e.avatar} alt={e.name} />
                         <h5 > {e.name}</h5>
-                    </div>
+                    </div>)
+
+                }
                 )}
             <button className='addToGroupButton' onClick={handlerAddParticipant}><p><MdOutlineGroupAdd /></p></button>
 
@@ -78,24 +71,24 @@ const Participants = ({ participants, id, refresh }) => {
                 </Modal.Header>
                 <Modal.Body>
                     {friends.length > 0 && friends.map(e => {
-
-                        return (
-                            < div className="cardRow" key={e._id} >
-                                <div class="d-flex text-align-center">
-                                    <img src={e.avatar} alt={e.name} />
-                                    <p>{e.name}</p>
+                        if (!participants.filter(elm => elm._id === e._id).length > 0) {
+                            return (
+                                < div className="cardRow" key={e._id} >
+                                    <div class="d-flex text-align-center">
+                                        <img src={e.avatar} alt={e.name} />
+                                        <p>{e.name}</p>
+                                    </div>
+                                    <div >
+                                        <button onClick={addMember} value={e._id} >add</button>
+                                    </div>
                                 </div>
-                                <div >
-                                    <button onClick={markChecked} value={e._id} >add</button>
-                                </div>
-                            </div>
-                        )
-
+                            )
+                        }
                     }
                     )}
                 </Modal.Body>
                 <Modal.Footer className='d-flex justify-content-around'>
-                    <Button className='myButton2' onClick={handleAddToGroup}>
+                    <Button className='myButton2' onClick={updateGroup}>
                         Añadir al grupo
                     </Button>
                     <Button className='myButton' onClick={handleClose}>
