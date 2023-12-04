e59648882788ba40805c4a4ade1c8177670ad005
Fabio García <fabiogcaam@gmail.com>
2023-12-04 19:04:43 +0100

    adjustments

--- src/components/Plan/Plan.jsx
+++ src/components/Plan/Plan.jsx
@@ -73,35 +73,35 @@ const Plan = ({ dates }) => {
     // useEffect(() => { getList() }, [state])
 
     return (
-        <div className='Plan'>
-            <div className='slider'>
-                <div className='icon'>
-                    <h5> <MdOutlineCalendarToday /></h5>
-                </div>
-                {dates.map(e => {
+        // <div className='Plan'>
+        //     <div className='slider'>
+        //         <div className='icon'>
+        //             <h5> <MdOutlineCalendarToday /></h5>
+        //         </div>
+        //         {dates.map(e => {
 
-                    return (
-                        <div className='fecha' key={e}>
-                            <h5>
-                                {shortDate(new Date(Date.parse(e)))}
-                            </h5>
-                        </div>
-                    )
+        //             return (
+        //                 <div className='fecha' key={e}>
+        //                     <h5>
+        //                         {shortDate(new Date(Date.parse(e)))}
+        //                     </h5>
+        //                 </div>
+        //             )
 
-                })}
+        //         })}
 
-                <Row className="row" style={{ color: '#011e3d', borderRadius: '5px', padding: "10px" }}>
-                    <Accordion >
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header style={{ backgroundColor: " #e5e9ec" }}><h3>Saved to the Plan</h3></Accordion.Header>
-                            <Accordion.Body>
-                                <SavedPlanRow myPlans={myPlans} />
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-                </Row>
-            </div>
-        </div >
+        <Row className="row" style={{ color: '#011e3d', borderRadius: '5px', padding: "10px" }}>
+            <Accordion >
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header style={{ backgroundColor: " #e5e9ec" }}><h3>Saved to the Plan</h3></Accordion.Header>
+                    <Accordion.Body>
+                        <SavedPlanRow myPlans={myPlans} />
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </Row>
+        //     </div>
+        // </div >
     )
 }
 export default Plan
